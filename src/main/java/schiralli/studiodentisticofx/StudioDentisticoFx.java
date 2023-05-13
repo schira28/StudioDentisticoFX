c9b94ef5349a9a03608a441e8bb0f838e228a18a
schira28 <116425771+schira28@users.noreply.github.com>
2023-05-13 18:23:10 +0200

    Modificati file
    - Cambiati .fxml
    - Cambiate proporzioni scena
    - Aggiunto nuovo titolo alla scena principale

--- src/main/java/schiralli/studiodentisticofx/StudioDentisticoFx.java
+++ src/main/java/schiralli/studiodentisticofx/StudioDentisticoFx.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class StudioDentisticoFx extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StudioDentisticoFx.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(StudioDentisticoFx.class.getResource("HomePage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Benvenuto in questo studio dentistico.");
         stage.setScene(scene);
         stage.show();
     }
